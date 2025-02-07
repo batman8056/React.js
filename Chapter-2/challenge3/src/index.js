85f0ae5d6b1c4ecccc97fa46319b959dc0f678df
batman8056 <manimanish8056@gmail.com>
2025-02-07 22:10:26 +0530

    challenge 3 completed

--- Chapter-2/challenge3/src/index.js
+++ Chapter-2/challenge3/src/index.js
@@ -1,16 +1,20 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
 import React from "react";
 import ReactDom from "react-dom";
 
-//using reactDom to render the content 
+const yourName = "Manikandan"
+const currentYear = new Date().getFullYear()
 ReactDom.render(
     <div>
-        <h1>Hello World</h1>
-        <p>This is my Paragraph</p>
-    </div>, 
-    document.getElementById("root"));
-
-// //another old way of javascript(pure Javascript)
-// var h1 = document.createElement("h1");
-// h1.innerHTML = "Hello world"
-// document.getElementById("root").appendChild(h1);
-
+        <p>Created by {yourName}.</p>
+        <p>Copyright {currentYear}</p>
+    </div>,document.getElementById("root")
+);
