e090df152d0999f1cfa84519a7c87f3ad39a78b6
batman8056 <manimanish8056@gmail.com>
2025-02-08 19:22:09 +0530

    challenge4 completed

---
+++ Chapter-3/challenge4/src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1>This is my first website</h1>
+            <p>this is my paragraph</p>
+        </div>
+    );
+};
+export default Note;
