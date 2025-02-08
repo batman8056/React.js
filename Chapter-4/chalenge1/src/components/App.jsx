07a97f13eb9b7e8b4021dcc20df268713a60e008
batman8056 <manimanish8056@gmail.com>
2025-02-08 22:03:28 +0530

    challenge1

---
+++ Chapter-4/chalenge1/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card.jsx";
+import contacts from  "../contacts.js";
+import Avator from "./Avator.jsx"
+function App(){
+    return (
+        <div>
+            <Avator img={Avator}/>
+            <Card name={contacts[0].name} img={contacts[0].imgURL} tel={contacts[0].tel} web={contacts[0].email} />
+            <Card name={contacts[1].name} img={contacts[1].imgURL} tel={contacts[1].tel} web={contacts[1].email} />
+            <Card name={contacts[2].name} img={contacts[2].imgURL} tel={contacts[2].tel} web={contacts[2].email} />
+        </div>
+    )
+};
+export default App;
---
+++ Chapter-4/chalenge1/src/components/Avator.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Avator(){
+    return(
+        <div>
+            <img className="circle-img" src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg" alt="avatar_img" />
+        </div>
+    )
+};
+
+export default Avator;
