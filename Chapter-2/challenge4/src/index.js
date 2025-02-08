94cf7a82016b65ab7eb215eff0438add61a462e6
batman8056 <manimanish8056@gmail.com>
2025-02-08 11:41:10 +0530

    styles notes commite

--- Chapter-2/challenge4/src/index.js
+++ Chapter-2/challenge4/src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-//using reactDom to render the content 
+const img = "https://picsum.photos/200"
 ReactDom.render(
     <div>
-        <h1>Hello World</h1>
-        <p>This is my Paragraph</p>
-    </div>, 
-    document.getElementById("root"));
-
-// //another old way of javascript(pure Javascript)
-// var h1 = document.createElement("h1");
-// h1.innerHTML = "Hello world"
-// document.getElementById("root").appendChild(h1);
+        <h1 className="heading" contentEditable="true" spellCheck="true">Hello world</h1>
+        <ul>
+      <li contentEditable="true" spellCheck="true">All days need progress</li>
+      <img className="success-img" src="/image/image1.png" alt="Progress" />
+      <li contentEditable="true" spellCheck="true">Eat healthy food</li>
+      <img className="success-img" src="/image/image2.png" alt="Healthy Food" />
+      <li contentEditable="true" spellCheck="true">Stay calm</li>
+      <img className="success-img" src="/image/image3.png" alt="Stay Calm" />
+    </ul>
+    <p>Just Refress Random Image will be appear </p>
+    <img src={img+ "?grayscale"} height="150px" width="200px" alt="random image"/>
+    </div>,
+    document.getElementById("root")
+);
 
