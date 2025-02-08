99230de28479732aa0a63d269d95b25ade24dbb4
batman8056 <manimanish8056@gmail.com>
2025-02-08 17:55:37 +0530

    update notes

---
+++ Chapter-3/challenge4/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+
+ReactDom.render(
+    <div>
+        <h1>Hello World</h1>
+        <p>This is my Paragraph</p>
+    </div>, 
+    document.getElementById("root")
+);
+
+
