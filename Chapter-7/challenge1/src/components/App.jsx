e8905c5b9f7b2b776757e0fb26c91a7f1d6cd92d
batman8056 <manimanish8056@gmail.com>
2025-02-12 10:39:38 +0530

    complex state handle

--- Chapter-7/challenge1/src/components/App.jsx
+++ Chapter-7/challenge1/src/components/App.jsx
@@ -1,23 +1,54 @@
 import React, {useState} from "react";
 
-
 function App() {
-  
-    const [currState, nextvalueState] = useState("")
-    const [currsubmittedState, nextsubmittedState] = useState("")
-    function clickButton(){
-      nextsubmittedState(currState);
-    }
-    function handleChange(event){
-      nextvalueState(event.target.value)
-    }
-    return (
-      <div className="container">
-        <h1>Hello {currsubmittedState}</h1>
-        <input onChange={handleChange} type="text" placeholder="First Name" />
-        <input onChange={handleChange} type="text" placeholder="Last Name" />
-        <button onClick={clickButton}>Submit</button>
-      </div>
-    );
+  const [fullName, setfullName] = useState({
+    fName:"",
+    lName:""
+  })
+  function handleChange(event){
+    const {value, name} =event.target
+    setfullName((prevValue)=>{
+      if(name === "fName"){
+        return {
+          fName: value,
+          lName: prevValue.lName
+        }
+      }else if(name === "lName"){
+        return {
+          fName: prevValue.fName,
+          lName: value
+        }
+      }
+    })
   }
-  export default App;
+  return (
+    <div className="container">
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
+      <input onChange={handleChange} name="fName" type="text" placeholder="First Name" value={fullName.fName}/>
+      <input onChange={handleChange} name="lName" type="text" placeholder="Last Name" value={fullName.lName}/>
+      <button>Submit</button>
+    </div>
+  );
+}
+export default App;
+
+//using multiple state to do opertaion
+// function App() {
+//     const [fName, setFname] = useState("")
+//     const [lName, setLname] = useState("")
+//     function handleChange(event){
+//       setFname(event.target.value)
+//     }
+//     function handleChange1(event){
+//       setLname(event.target.value);
+//     }
+//     return (
+//       <div className="container">
+//         <h1>Hello {fName} {lName}</h1>
+//         <input onChange={handleChange} type="text" placeholder="First Name" value={fName}/>
+//         <input onChange={handleChange1} type="text" placeholder="Last Name" value={lName}/>
+//         <button>Submit</button>
+//       </div>
+//     );
+//   }
+//   export default App;
